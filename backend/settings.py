408f86fabbf6a1087f89449998aa9b2c058a7fdb
Sanskar <sanskarjha007@gmail.com>
2020-12-22 05:01:03 +0530

    Changed Allowed Hosts

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'uvm@y+88#8+9%r8a$6&ubff-w6dcrr%r#b@7j(1ep$vpte+0c&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['sanskar-fyle-challenge-backend.herokuapp.com', '127.0.0.1']
 # ALLOWED_HOSTS = []
 
 
@@ -152,3 +152,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join.(BASE_DIR, 'static')
